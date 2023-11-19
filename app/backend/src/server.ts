75001e9f884234080bcfd9e625d307a2d4c3ef72
Samson Ajulor <samsonajulor@gmail.com>
2023-11-19 18:21:00 +0100

    registration routes now working

--- app/backend/src/server.ts
+++ app/backend/src/server.ts
@@ -2,13 +2,15 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import { customAlphabet } from 'nanoid';
-import { numbers } from 'nanoid-dictionary';
+import dictionary from 'nanoid-dictionary';
 import { env, sendEmail, logger } from './utils/index.ts';
-import { saveCache, cacheExists } from './db/cache.ts';
+import { saveCache, cacheExists, getCache } from './db/cache.ts';
 
 const app = express();
 
 const PORT = env.PORT || 6998;
+
+const { numbers } = dictionary;
 
 const nanoid = customAlphabet(numbers, 6);
 
@@ -26,16 +28,41 @@ app.post('/register', (req, res) => {
       return res.status(400).json({ error: 'Invalid email address' });
     }
 
-    if (cacheExists(email)) {
-      return res.status(400).json({ error: 'Email already registered' });
-    }
-
-    saveCache(email, email);
+    saveCache(String(otp), email);
 
     let message = `Your verification code is ${otp}`;
     sendEmail(email, 'Email Verification', message);
 
-    return res.status(200).json({ message: 'Email saved successfully' });
+    return res.status(200).json({ message: 'Otp has been sent. Please check your email.', otp });
+  } catch (error: any) {
+    logger('error', error.message || error.toString());
+    return res.status(500).json({ error: error.message || 'something really bad happened.' });
+  }
+});
+
+app.post('/validate-otp', (req, res) => {
+  try {
+    const { otp, email } = req.body;
+    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ error: 'Invalid email address' });
+    }
+
+    if (!otp) {
+      return res.status(400).json({ error: 'Invalid otp. You will need to generate one.' });
+    }
+
+    const otpInCache = cacheExists(String(otp));
+
+    const otpData = getCache(otp) as unknown as string;
+
+    logger('is otp in cache?', JSON.stringify(otpInCache));
+
+    if (!otpInCache || otpData !== email) {
+      return res.status(400).json({ error: 'Invalid otp. You will need to generate one.' });
+    }
+
+    return res.status(200).json({ message: 'Access Granted.' });
   } catch (error: any) {
     logger('error', error.message || error.toString());
     return res.status(500).json({ error: error.message || 'something really bad happened.' });
