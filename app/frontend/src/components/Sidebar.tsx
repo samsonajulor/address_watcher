d535e66b3c325b7e283b8cdc6ac0357395479c78
james-o <jaydhales@gmail.com>
2023-11-19 21:28:26 +0100

    update

--- app/frontend/src/components/Sidebar.tsx
+++ app/frontend/src/components/Sidebar.tsx
@@ -1,7 +1,7 @@
 import { useRef, useEffect } from 'react';
 import { useMainContext } from '../contexts/MainContext';
 import { NavLink } from 'react-router-dom';
-import { useOnClickOutside, useWindowSize } from 'usehooks-ts';
+import { useDarkMode, useOnClickOutside, useWindowSize } from 'usehooks-ts';
 import { MdSpaceDashboard } from 'react-icons/md';
 import { GrTransaction } from 'react-icons/gr';
 import { IoMdSettings } from 'react-icons/io';
@@ -9,6 +9,7 @@ import { useMediaQuery } from 'usehooks-ts';
 
 const Sidebar = () => {
   const { navbarOpen, setNavbarOpen } = useMainContext();
+  const { isDarkMode, toggle, enable, disable } = useDarkMode();
   const ref = useRef(null);
   const isLargeScreen = useMediaQuery('(min-width: 1260px)');
   const isSmallScreen = useMediaQuery('(max-width: 1024px)');
@@ -33,27 +34,29 @@ const Sidebar = () => {
       ref={ref}
       className={` ${
         navbarOpen ? 'fixed w-52 h-max lg:static lg:w-auto z-50 lg:mt-8' : 'hidden'
-      } bar flex flex-col justify-center gap-8 py-10 min-h-[calc(100vh-200px)]`}
+      } bar `}
     >
-      {[
-        ['/app', <MdSpaceDashboard />, 'Dashboard'],
-        ['/app/activity', <GrTransaction />, 'Activity'],
-        ['/app/settings', <IoMdSettings />, 'Settings'],
-      ].map((path) => (
-        <NavLink
-          to={path[0] as string}
-          key={path[0] as string}
-          className={({ isActive }) =>
-            `flex gap-2 items-center btn ${
-              isActive ? 'bg-cs-light-purple text-cs-primary' : 'hover:bg-cs-dark-bg/10'
-            }`
-          }
-          end
-        >
-          {(isSmallScreen || isLargeScreen) && path[1]}
-          {path[2]}
-        </NavLink>
-      ))}
+      <div className="flex flex-col justify-center gap-8 py-10 min-h-[calc(100vh-200px)] w-full relative">
+        {[
+          ['/app', <MdSpaceDashboard />, 'Dashboard'],
+          ['/app/activity', <GrTransaction />, 'Activity'],
+          ['/app/settings', <IoMdSettings />, 'Settings'],
+        ].map((path) => (
+          <NavLink
+            to={path[0] as string}
+            key={path[0] as string}
+            className={({ isActive }) =>
+              `flex gap-2 items-center btn ${
+                isActive ? 'bg-cs-light-purple text-cs-primary' : 'hover:bg-cs-dark-bg/10'
+              }`
+            }
+            end
+          >
+            {(isSmallScreen || isLargeScreen) && path[1]}
+            {path[2]}
+          </NavLink>
+        ))}
+      </div>
     </div>
   );
 };
