c87e5979033712d5be32383d76297ad5abea06c9
james-o <jaydhales@gmail.com>
2023-11-21 17:27:13 +0100

    Decoder

---
+++ app/frontend/src/constants/Contract_Interfaces.ts
@@ -0,0 +1,25 @@
+export const IERC20 = [
+  'name',
+  'approve',
+  'totalSupply',
+  'transferFrom',
+  'decimals',
+  'balanceOf',
+  'symbol',
+  'transfer',
+  'allowance',
+];
+
+export const IERC721 = [
+  'approve',
+  'mint',
+  'safeTransferFrom',
+  'safeTransferFrom',
+  'setApprovalForAll',
+  'transferFrom',
+  'balanceOf',
+  'getApproved',
+  'isApprovedForAll',
+  'ownerOf',
+  'supportsInterface',
+];
--- app/frontend/src/pages/App/components/Pagination.tsx
+++ app/frontend/src/pages/App/components/Pagination.tsx
@@ -3,27 +3,34 @@ import React from 'react';
 interface PaginationProps {
   previousPage: () => void;
   nextPage: () => void;
+  disableFirst: boolean;
+  disableLast: boolean;
 }
 
-const Pagination: React.FC<PaginationProps> = ({ previousPage, nextPage }) => {
+const Pagination: React.FC<PaginationProps> = ({
+  previousPage,
+  nextPage,
+  disableFirst,
+  disableLast,
+}) => {
   return (
     <nav aria-label="Page navigation example" className="align-right my-5">
       <ul className="flex items-center text-base">
         <li onClick={previousPage}>
-          <a
-            href="#"
-            className="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          <button
+            disabled={disableFirst}
+            className="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white "
           >
             Previous
-          </a>
+          </button>
         </li>
         <li onClick={nextPage}>
-          <a
-            href="#"
-            className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          <button
+            disabled={disableLast}
+            className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white disabled:opacity-30"
           >
             Next
-          </a>
+          </button>
         </li>
       </ul>
     </nav>
